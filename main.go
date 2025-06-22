b09a9a01a6d03ebd0d97e0a1573e4ab4228f15db
VERA-OBIERO <vera.obiero@gmail.com>
2025-06-22 20:53:31 +0300

    function to allow user to enter their name, the song they want and the artist of the song

--- main.go
+++ main.go
@@ -7,9 +7,29 @@ var djName = "Bubbles"
 
 func main() {
     greetUser()
+    requestSong()
 }
 
 func greetUser()(string, string){
     fmt.Printf("Welcome to %v!\nThis is DJ %v, ready to make you dance! \n", appName, djName)
     return appName, djName
+}
+
+func requestSong()(string, string, string){
+    var userName string
+    var songName string
+    var artistName string
+
+    fmt.Printf("Please enter your name: ")
+    fmt.Scanln(&userName)
+
+    fmt.Printf("Please enter the name of the song: ")
+    fmt.Scanln(&songName)
+
+    fmt.Printf("Please enter the artist of the song: ")
+    fmt.Scanln(&artistName)
+
+    fmt.Printf("Hi %v, you have requested for %v by %v\n", userName, songName, artistName)
+    return userName, songName, artistName
+
 }
